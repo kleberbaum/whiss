62ecc8b51b0fd2a81181f9f6920af6628c82cb84
Florian Kleber <kleber@snek.at>
2024-09-22 03:55:36 +0000

    chore: use npm create pylon and adjust
    This adds the necessary dependencies and configurations for the project.
    The changes include adding the @getcronit/pylon package as a dependency,
    updating the scripts section in package.json, and adding the types
    option in tsconfig.json.

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ *
+ * SPDX-FileType: SOURCE
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Florian Kleber
+ *
+ * Use of this source code is governed by an MIT license that can be found
+ * in the LICENSE file at https://github.com/kleberbaum/whiss/blob/main/LICENSES/preferred/EUPL-1.2
+ */
+import { app } from '@getcronit/pylon'
+
+export const graphql = {
+  Query: {
+    sum: (a: number, b: number) => {
+      console.log(`Calculating sum of ${a} and ${b}`)
+      return a + b
+    }
+  },
+  Mutation: {
+    divide: (a: number, b: number) => {
+      if (b === 0) {
+        console.error('Attempt to divide by zero')
+        throw new Error('Division by zero is not allowed')
+      }
+      console.log(`Dividing ${a} by ${b}`)
+      return a / b
+    }
+  }
+}
+
+export default app;
